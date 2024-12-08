63587a702e47ece5a4655ff0da0a4ae23d0bc906
mrtmnr <mertmunar@gmail.com>
2024-12-08 12:47:50 +0300

    securityConfig created

--- src/main/java/com/sau/learningplatform/Controller/ProjectController.java
+++ src/main/java/com/sau/learningplatform/Controller/ProjectController.java
@@ -5,7 +5,7 @@ import com.sau.learningplatform.Service.ProjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
